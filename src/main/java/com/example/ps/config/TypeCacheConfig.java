21850d7a23ea4deb80224ac6e16412afce4fe265
rabindra patra <docs.rabindrapatra@gmail.com>
2025-11-12 10:04:50 +0530

    Create TypeCacheConfig.java

---
+++ src/main/java/com/example/ps/config/TypeCacheConfig.java
@@ -0,0 +1,7 @@
+package com.example.ps.config;
+
+public record TypeCacheConfig(  String name,
+                                int capacity,
+                                int count,
+                                boolean enabled) {
+}
